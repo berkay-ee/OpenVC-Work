3b53ce4e42dbdccae4a014c02afc4bfe44d435bc
berkay-ee <berkayaydin.ee@gmail.com>
2025-10-04 00:45:41 +0300

    gray scale add

--- Rust_detection.py
+++ Rust_detection.py
@@ -2,13 +2,22 @@ import cv2
 import numpy as np
 
 
-blank = np.zeros((500, 500, 3), dtype=np.uint8)
+
+img = cv2.imread("Rust Detection/rust_bridge.jpg") 
+
+gray_convert  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+print("Pixel before:", gray_convert[160, 100])
+gray_convert[100:160, 50:100] = 255
+print("Pixel after:", gray_convert[160, 100])
 
 
-cv2.imshow("Empty Screen", blank)
+# Show the image in a window
+cv2.imshow("My JPG Picture", gray_convert)
 
-
+# Wait until a key is pressed
 cv2.waitKey(0)
 
-
+# Close all windows
 cv2.destroyAllWindows()
+
