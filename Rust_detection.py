8e3621d1bf0cd053c10d710510a6632b08c4c61d
berkay-ee <berkayaydin.ee@gmail.com>
2025-10-04 17:48:51 +0300

    Rust detected with a basic surface

--- Rust_detection.py
+++ Rust_detection.py
@@ -3,18 +3,38 @@ import numpy as np
 
 
 #Reading from the image file
-img = cv2.imread("Rust Detection/rust_bridge.jpg") 
+img = cv2.imread("rust_surface.jpg") 
 
 #Convert the gray scale
 gray_convert  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-print("Pixel before:", gray_convert[160, 100])
-gray_convert[100:160, 50:100] = 255
-print("Pixel after:", gray_convert[160, 100])
+orange = (0, 165, 255)  # BGR orange
+
+rows, cols = gray_convert.shape
+
+
+# Thershold the image
+#if greher than thershold make a array point is a black is less than theshold make a array point white at the gray scale
+for i in range(rows):
+    for j in range(cols):
+        if gray_convert[i,j] < 75:
+          gray_convert[i,j] = 0
+        else:
+            gray_convert[i,j] = 255
+
+
+# point the rust locaiton at the RGB image
+for i in range(rows):
+    for j in range(cols):
+        if gray_convert[i,j] == 0:
+          cv2.circle(img, (j,i),1,orange,-1)
+        
 
 
 # Show the image in a window
-cv2.imshow("My JPG Picture", gray_convert)
+cv2.imshow("Rust detected picture", img)
+
+cv2.imshow("Gray scale", gray_convert)
 
 # Wait until a key is pressed
 cv2.waitKey(0)
